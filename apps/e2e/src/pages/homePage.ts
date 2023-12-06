6f08ff013fc822eaa82550db9a63f4f600a3fde0
Maciej Cieslar <maciejcieslar@divante.pl>
2023-12-06 20:05:25 +0100

    feat: add basic setup

--- apps/e2e/src/pages/homePage.ts
+++
@@ -1,9 +0,0 @@
-import { type Page } from '@playwright/test';
-
-export class HomePage {
-  public constructor(private readonly page: Page) {}
-
-  public async goto(): Promise<void> {
-    await this.page.goto('/');
-  }
-}
--- apps/e2e/vitest.config.js
+++
@@ -1,6 +0,0 @@
-import { mergeConfig } from 'vite';
-import { defineConfig } from 'vitest/config';
-
-import vitestConfig from '../../vitest.config';
-
-export default mergeConfig(vitestConfig, defineConfig({}));
