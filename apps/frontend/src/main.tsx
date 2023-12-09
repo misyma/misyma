13cfb75da1caeea99649e7627b17457c36ead387
Maciej Cieślar <maciejmcieslar@gmail.com>
2023-12-10 00:56:31 +0100

    feat: add hooks (#16)
    * feat: add hooks
    
    * feat: add knip and fix docker
    
    ---------
    
    Co-authored-by: Maciej Cieslar <maciejcieslar@divante.pl>

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,19 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './App.tsx';
 import './index.css';
-
-async function enableMocking() {
-  if (process.env.NODE_ENV !== 'development') {
-    return;
-  }
-
-  const { worker } = await import('./mocks/worker.ts');
-
-  return await worker.start();
-}
-
-await enableMocking();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
