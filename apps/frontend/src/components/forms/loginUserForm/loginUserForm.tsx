7f2768c13ff01ee72d928f5b936cb17708ffa6a9
Jakub Barczewski <jakub_barczewski@protonmail.com>
2024-02-25 13:19:36 +0100

    fix: final invalid reference, try fix turbo

--- apps/frontend/src/components/forms/loginUserForm/loginUserForm.tsx
+++ apps/frontend/src/components/forms/loginUserForm/loginUserForm.tsx
@@ -7,7 +7,7 @@ import { useForm } from 'react-hook-form';
 import { LoginUserFormValues, loginUserFormSchema } from './schema/loginUserFormSchema';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useLoginUserMutation } from '../../../api/user/mutations/loginUserMutation/loginUserMutation';
-import { LoginUserResponseBody } from '../../../../../../common/contracts/dist/src/schemas/user/loginUser';
+import { LoginUserResponseBody } from '@common/contracts';
 import { UserApiError } from '../../../api/user/errors/userApiError';
 
 interface LoginUserFormProps {
