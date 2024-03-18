d466b59874d3663da6bf73a2204da1b89b47337d
Jakub Barczewski <jakub_barczewski@protonmail.com>
2024-03-18 23:04:24 +0100

    fix: broken fe after changes

--- apps/frontend/src/api/books/bookReadings/addBookReadingMutation/addBookReadingMutation.ts
+++ apps/frontend/src/api/books/bookReadings/addBookReadingMutation/addBookReadingMutation.ts
@@ -1,11 +1,19 @@
 import { UseMutationOptions, useMutation } from '@tanstack/react-query';
-import { BookReading, CreateBookReadingRequestBody, CreateBookReadingPathParams, CreateBookReadingResponseBody } from '@common/contracts';
+import {
+  BookReading,
+  CreateBookReadingRequestBody,
+  CreateBookReadingPathParams,
+  CreateBookReadingResponseBody,
+} from '@common/contracts';
 import { HttpService } from '../../../../core/services/httpService/httpService';
 import { BookApiError } from '../../errors/bookApiError';
 import { useSelector } from 'react-redux';
 import { userStateSelectors } from '../../../../core/store/states/userState/userStateSlice';
 
-type AddBookReadingMutationPayload = CreateBookReadingRequestBody & CreateBookReadingPathParams;
+type AddBookReadingMutationPayload = CreateBookReadingRequestBody &
+  CreateBookReadingPathParams & {
+    userId: string;
+  };
 
 export const useAddBookReadingMutation = (
   options?: UseMutationOptions<BookReading, BookApiError, AddBookReadingMutationPayload, unknown>,
@@ -13,12 +21,14 @@ export const useAddBookReadingMutation = (
   const accessToken = useSelector(userStateSelectors.selectAccessToken);
 
   const addBookReading = async (payload: AddBookReadingMutationPayload) => {
+    const { userId, userBookId, ...body } = payload;
+
     const response = await HttpService.post<CreateBookReadingResponseBody>({
-      url: `/books/${payload.bookId}/book-readings`,
-      body: payload as unknown as Record<string, unknown>,
+      url: `/users/${userId}/books/${userBookId}/readings`,
+      body: body as unknown as Record<string, unknown>,
       headers: {
         Authorization: `Bearer ${accessToken}`,
-      }
+      },
     });
 
     if (response.success === false) {
--- apps/frontend/src/api/books/queries/findBooksByBookshelfId/findBooksByBookshelfIdQuery.ts
+++ apps/frontend/src/api/books/queries/findBooksByBookshelfId/findBooksByBookshelfIdQuery.ts
@@ -1,32 +1,23 @@
 import { useSelector } from 'react-redux';
 import { userStateSelectors } from '../../../../core/store/states/userState/userStateSlice';
-import { FindBooksByBookshelfIdPathParams, FindBooksByBookshelfIdResponseBody } from '@common/contracts';
-import { HttpService } from '../../../../core/services/httpService/httpService';
 import { useQuery } from '@tanstack/react-query';
+import { findBooksByBookshelfId } from './findBooksByBookshelfId';
 
-export const useFindBooksByBookshelfIdQuery = (bookshelfId: string) => {
+type Payload = {
+  bookshelfId: string;
+  userId: string;
+}
+
+export const useFindBooksByBookshelfIdQuery = ({ bookshelfId, userId }: Payload) => {
   const accessToken = useSelector(userStateSelectors.selectAccessToken);
 
-  const findBooksByBookshelfId = async (values: FindBooksByBookshelfIdPathParams) => {
-    const { bookshelfId } = values;
-
-    const response = await HttpService.get<FindBooksByBookshelfIdResponseBody>({
-      url: `/books/bookshelf/${bookshelfId}`,
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-
-    if (response.success === false) {
-      throw new Error('Error');
-    }
-
-    return response.body;
-  };
-
-    return useQuery<FindBooksByBookshelfIdResponseBody>({
-        queryKey: ['findBooksByBookshelfId', bookshelfId],
-        queryFn: () => findBooksByBookshelfId({ bookshelfId }),
-        enabled: !!accessToken || !!bookshelfId,
-    })
+  return useQuery({
+    queryKey: ['findBooksByBookshelfId', bookshelfId, userId],
+    queryFn: () => findBooksByBookshelfId({ 
+      bookshelfId,
+      accessToken: accessToken as string,
+      userId
+    }),
+    enabled: !!accessToken || !!bookshelfId,
+  });
 };
