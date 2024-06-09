dddb06ca6ae41759955e57f7488e6444d1f28f8b
Jakub Barczewski <jakub_barczewski@protonmail.com>
2024-06-09 22:56:39 +0200

    fix: missing return statement

--- apps/frontend/src/routes/shelves/shelves.tsx
+++ apps/frontend/src/routes/shelves/shelves.tsx
@@ -320,7 +320,7 @@ export const ShelvesPage: FC = () => {
                               className="pointer-events-auto text-primary h-8 w-8 cursor-pointer"
                               onClick={() => {
                                 if (bookshelves && bookshelves[index]?.id === '') {
-                                  onCreateNew(index);
+                                  return onCreateNew(index);
                                 }
 
                                 onSaveEdit(index);
