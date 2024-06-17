f97b914d4527c9e00c073435c2e35c227b7e90b4
Jakub Barczewski <jakub_barczewski@protonmail.com>
2024-06-17 18:57:29 +0200

    chore: add always-refreshing-image & fix http router

--- apps/frontend/src/modules/bookshelf/components/createBookForm/steps/stepThree/manualStepThreeForm.tsx
+++ apps/frontend/src/modules/bookshelf/components/createBookForm/steps/stepThree/manualStepThreeForm.tsx
@@ -192,7 +192,6 @@ export const ManualStepThreeForm = ({ bookshelfId }: Props): JSX.Element => {
           ...(bookCreation.stepTwoDetails as Required<BookCreationNonIsbnState['stepTwoDetails']>),
           ...(bookCreation.stepThreeDetails as Required<BookCreationNonIsbnState['stepThreeDetails']>),
           ...(bookCreation.stepOneDetails as Required<BookCreationNonIsbnState['stepOneDetails']>),
-          isbn: bookCreation.stepOneDetails?.isbn === '' ? undefined : bookCreation.stepOneDetails?.isbn,
           accessToken: accessToken as string,
         });
       } catch (error) {
