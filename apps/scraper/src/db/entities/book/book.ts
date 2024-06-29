afbdbdf37a85504e8da648033bf2a484aa77b918
Michał Cieślar <michalovskyyy@gmail.com>
2024-06-29 23:04:19 +0100

    feat: add scraper cli setup (#345)

---
+++ apps/scraper/src/db/entities/book/book.ts
@@ -0,0 +1,16 @@
+import { type BookFormat } from './bookFormat.js';
+import { type Language } from './language.js';
+
+export interface Book {
+  readonly id: string;
+  readonly title: string;
+  readonly isbn?: string | undefined;
+  readonly publisher?: string | undefined;
+  readonly releaseYear?: number | undefined;
+  readonly language: Language;
+  readonly translator?: string | undefined;
+  readonly format: BookFormat;
+  readonly pages?: number | undefined;
+  readonly isApproved: boolean;
+  readonly imageUrl?: string | undefined;
+}
--- apps/scraper/src/index.ts
+++ apps/scraper/src/index.ts
@@ -1,6 +1,16 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import yargs, { type Argv } from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+import { ScrapeOpenLibraryAction } from './actions/scrapeOpenLibraryAction.js';
 import { ConfigFactory } from './config.js';
+import { AuthorRepository } from './db/repositories/authorRepository/authorRepository.js';
 import { BaseError } from './errors/baseError.js';
+import { DatabaseClientType } from './libs/database/databaseClientConfig.js';
+import { DatabaseClientFactory } from './libs/database/databaseClientFactory.js';
 import { LoggerServiceFactory } from './libs/logger/loggerServiceFactory.js';
+import { UuidService } from './libs/uuid/uuidService.js';
 
 const finalErrorHandler = async (error: unknown): Promise<void> => {
   let errorContext;
@@ -9,7 +19,6 @@ const finalErrorHandler = async (error: unknown): Promise<void> => {
     errorContext = {
       name: error.name,
       message: error.message,
-      stack: error.stack,
       ...(error instanceof BaseError ? { ...error.context } : undefined),
     };
   } else {
@@ -23,7 +32,7 @@ const finalErrorHandler = async (error: unknown): Promise<void> => {
     }),
   );
 
-  process.exitCode = 1;
+  process.exit(1);
 };
 
 process.on('unhandledRejection', finalErrorHandler);
@@ -39,7 +48,34 @@ try {
 
   const logger = LoggerServiceFactory.create(config);
 
-  logger.info({ message: 'Application started.' });
+  const dbClient = DatabaseClientFactory.create({
+    clientType: DatabaseClientType.sqlite,
+    filePath: config.databasePath,
+    useNullAsDefault: true,
+    minPoolConnections: 1,
+    maxPoolConnections: 1,
+  });
+
+  const uuidService = new UuidService();
+
+  const authorRepository = new AuthorRepository(dbClient, uuidService);
+
+  const scrapeOpenLibraryAction = new ScrapeOpenLibraryAction(authorRepository, logger);
+
+  yargs(hideBin(process.argv))
+    .command([
+      {
+        command: 'scrape openlibrary',
+        describe: 'Scrape resources from Open Library.',
+        builder: (builder: any): Argv => {
+          return builder.usage('Usage: scraper scrape openlibrary');
+        },
+        handler: async (): Promise<void> => {
+          await scrapeOpenLibraryAction.execute();
+        },
+      },
+    ])
+    .help().argv;
 } catch (error) {
   await finalErrorHandler(error);
 }
