762acaf67ee828328a16b9763fb71896aecb3af4
Michal Cieslar <michalovskyyy@gmail.com>
2024-07-09 00:23:36 +0200

    add mapping openlibrary language

--- apps/scraper/src/actions/scrapeOpenLibraryAction/openLibraryMapper.ts
+++ apps/scraper/src/actions/scrapeOpenLibraryAction/openLibraryMapper.ts
@@ -1,3 +1,5 @@
+import { isValidLanguage, type Language } from '../../db/entities/book/language.js';
+
 export class OpenLibraryMapper {
   public mapAuthorName(openLibraryAuthorName: string): string {
     const nameParts = openLibraryAuthorName.split(',');
@@ -7,5 +9,19 @@ export class OpenLibraryMapper {
     }
 
     return `${String(nameParts[1]).trimStart()} ${String(nameParts[0])}`;
+  }
+
+  public mapLanguage(openLibraryLanguage: string): Language | undefined {
+    const abbreviatedLanguage = openLibraryLanguage.toLowerCase().substring(0, 2);
+
+    if (abbreviatedLanguage.length !== 2) {
+      return undefined;
+    }
+
+    if (isValidLanguage(abbreviatedLanguage)) {
+      return abbreviatedLanguage;
+    }
+
+    return undefined;
   }
 }
--- apps/scraper/src/actions/scrapeOpenLibraryAction/scrapeOpenLibraryAction.ts
+++ apps/scraper/src/actions/scrapeOpenLibraryAction/scrapeOpenLibraryAction.ts
@@ -1,10 +1,13 @@
-import lineByLine from 'n-readlines';
+import { createReadStream } from 'fs';
+import { createInterface } from 'readline';
 
 import { type OpenLibraryBook } from './openLibraryBook.js';
 import { type OpenLibraryMapper } from './openLibraryMapper.js';
 import { type AuthorRepository } from '../../db/repositories/authorRepository/authorRepository.js';
 import { type BookRepository } from '../../db/repositories/bookRepository/bookRepository.js';
 import { type LoggerService } from '../../libs/logger/loggerService.js';
+
+const languages = new Set<string>();
 
 export class ScrapeOpenLibraryAction {
   private readonly openLibraryDumpLocation = '/home/michal/Desktop/openlib_dump.jsonl';
@@ -20,70 +23,77 @@ export class ScrapeOpenLibraryAction {
     this.logger.info({
       message: 'Scraping Open Library...',
       bookRepository: this.bookRepository,
+      authorRepository: this.authorRepository,
+      openLibraryMapper: this.openLibraryMapper,
     });
 
-    const liner = new lineByLine(this.openLibraryDumpLocation);
+    await this.processFile(this.openLibraryDumpLocation);
 
-    console.log({ liner });
+    // const openLibraryAuthorNames = openLibraryBook.authors;
 
-    let line;
+    // if (!openLibraryAuthorNames || !openLibraryAuthorNames.length) {
+    //   this.logger.info({
+    //     message: 'Skipping book without authors.',
+    //     lineNumber,
+    //   });
 
-    let lineNumber = 0;
+    //   continue;
+    // }
 
-    const languages = new Set<string>();
+    // const authorNames = openLibraryAuthorNames
+    //   .filter((openLibraryAuthorName) => openLibraryAuthorName.length)
+    //   .map((openLibraryAuthorName) => this.openLibraryMapper.mapAuthorName(openLibraryAuthorName));
 
-    while ((line = liner.next())) {
-      const openLibraryBook = JSON.parse(line.toString()) as OpenLibraryBook;
+    // const authorIds = [];
 
-      lineNumber++;
+    // for (const authorName of authorNames) {
+    //   const author = await this.authorRepository.findAuthor({ name: authorName });
 
-      this.logger.info({
-        message: 'Processing book...',
-        lineNumber,
-        openLibraryBook,
-        authorRepository: this.authorRepository,
-        bookRepository: this.bookRepository,
-        openLibraryMapper: this.openLibraryMapper,
-      });
+    //   if (!author) {
+    //     const createdAuthor = await this.authorRepository.create({ name: authorName });
 
-      if (openLibraryBook.language) {
-        languages.add(openLibraryBook.language);
-      }
-
-      // const openLibraryAuthorNames = openLibraryBook.authors;
-
-      // if (!openLibraryAuthorNames || !openLibraryAuthorNames.length) {
-      //   this.logger.info({
-      //     message: 'Skipping book without authors.',
-      //     lineNumber,
-      //   });
-
-      //   continue;
-      // }
-
-      // const authorNames = openLibraryAuthorNames
-      //   .filter((openLibraryAuthorName) => openLibraryAuthorName.length)
-      //   .map((openLibraryAuthorName) => this.openLibraryMapper.mapAuthorName(openLibraryAuthorName));
-
-      // const authorIds = [];
-
-      // for (const authorName of authorNames) {
-      //   const author = await this.authorRepository.findAuthor({ name: authorName });
-
-      //   if (!author) {
-      //     const createdAuthor = await this.authorRepository.create({ name: authorName });
-
-      //     authorIds.push(createdAuthor.id);
-      //   } else {
-      //     authorIds.push(author.id);
-      //   }
-      // }
-    }
+    //     authorIds.push(createdAuthor.id);
+    //   } else {
+    //     authorIds.push(author.id);
+    //   }
+    // }
 
     console.log({ languages });
 
     this.logger.info({
       message: 'Scraping Open Library completed.',
     });
+  }
+
+  private async processFile(filePath: string): Promise<void> {
+    const rl = createInterface({
+      input: createReadStream(filePath),
+      crlfDelay: Infinity,
+    });
+
+    rl.on('line', (line: string) => {
+      this.processLine(line); // Process each line
+    });
+
+    await new Promise((resolve, reject) => {
+      rl.on('close', resolve);
+
+      rl.on('error', reject);
+    });
+
+    console.log('File processing completed.');
+  }
+
+  private async processLine(line: string): Promise<void> {
+    const openLibraryBook = JSON.parse(line.toString()) as OpenLibraryBook;
+
+    this.logger.info({
+      message: 'Processing book...',
+      language: openLibraryBook.language,
+    });
+
+    if (openLibraryBook.language) {
+      languages.add(openLibraryBook.language);
+    }
   }
 }
