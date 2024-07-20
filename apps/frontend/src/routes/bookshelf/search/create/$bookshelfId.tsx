e162075e2d2a25fc8a0c5a08dbf422818c85d397
Jakub Barczewski <jakub_barczewski@protonmail.com>
2024-07-20 19:55:22 +0200

    fix: add missing image to by title create

--- apps/frontend/src/routes/bookshelf/search/create/$bookshelfId.tsx
+++ apps/frontend/src/routes/bookshelf/search/create/$bookshelfId.tsx
@@ -10,7 +10,7 @@ export const SearchCreatePage: FC = () => {
   const searchBookContext = useSearchBookContext();
 
   // TODO: fix
-  const params = Route.useParams() as { bookshelfId: string};
+  const params = Route.useParams() as { bookshelfId: string };
 
   const navigate = useNavigate();
 
@@ -58,24 +58,35 @@ export const SearchCreatePage: FC = () => {
 
   return (
     <AuthenticatedLayout>
-      <div className="flex flex-col-reverse sm:px-10 pt-8 sm:pt-24 sm:flex-row gap-10 sm:gap-20 md:gap-30 lg:gap-60 max-w-[15rem] sm:max-w-[unset]">
-        <div className="sm:min-h-[40rem]">
-          <Breadcrumbs
-            crumbs={{
-              [1]: <NumericBreadcrumb index={1}>1</NumericBreadcrumb>,
-              [2]: <NumericBreadcrumb index={2}>2</NumericBreadcrumb>,
-              [3]: (
-                <NumericBreadcrumb
-                  index={3}
-                  className="font-semibold bg-primary text-white border-primary"
-                >
-                  3
-                </NumericBreadcrumb>
-              ),
-            }}
-          />
-          {}
-          <ManualStep bookshelfId={params.bookshelfId} />
+      <div className='w-full flex items-center justify-center'>
+        <div className="flex flex-col-reverse sm:px-10 pt-8 sm:pt-24 sm:flex-row gap-10 sm:gap-20 md:gap-30 lg:gap-60 max-w-[15rem] sm:max-w-[unset]">
+          <div className="sm:min-h-[40rem]">
+            <Breadcrumbs
+              crumbs={{
+                [1]: <NumericBreadcrumb index={1}>1</NumericBreadcrumb>,
+                [2]: <NumericBreadcrumb index={2}>2</NumericBreadcrumb>,
+                [3]: (
+                  <NumericBreadcrumb
+                    index={3}
+                    className="font-semibold bg-primary text-white border-primary"
+                  >
+                    3
+                  </NumericBreadcrumb>
+                ),
+              }}
+            />
+            {}
+            <ManualStep bookshelfId={params.bookshelfId} />
+          </div>
+          <div className="flex items-center justify-center">
+            <div className="relative w-full flex justify-center items-center h-[500px] md:h-[300px]">
+              <img
+                src="/books.png"
+                alt="Books image"
+                className="object-contain w-full h-full"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </AuthenticatedLayout>
