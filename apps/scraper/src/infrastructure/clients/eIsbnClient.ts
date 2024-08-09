8ca3409ef4656e33dbb02fe6e333253fb40bdfb6
Michal Cieslar <michalovskyyy@gmail.com>
2024-08-09 15:08:08 +0200

    fix: improve eisbn service error handling

--- apps/scraper/src/infrastructure/clients/eIsbnClient.ts
+++ apps/scraper/src/infrastructure/clients/eIsbnClient.ts
@@ -17,12 +17,7 @@ export class EIsbnClientFactory {
       timeout: 30000,
     });
 
-    axiosRetry(httpClient, {
-      retries: 5,
-      validateResponse: (response) => {
-        return !response.data.includes('Container has suffered a SystemException');
-      },
-    });
+    axiosRetry(httpClient, { retries: 3 });
 
     return httpClient;
   }
