0921fe6b1968e0400da12afb3358babe4c03fb5d
Jakub Barczewski <86159966+DeutscherDude@users.noreply.github.com>
2024-09-01 15:21:07 +0200

    Fix/weird nan issue with pages count (#541)
    * fix: weird nan assignment
    
    * chore: remove console log

--- apps/frontend/src/modules/book/components/adminCreateBookForm/steps/stepTwo/manualStepTwoForm.tsx
+++ apps/frontend/src/modules/book/components/adminCreateBookForm/steps/stepTwo/manualStepTwoForm.tsx
@@ -97,7 +97,9 @@ export const ManualStepTwoForm: FC<Props> = ({ onSubmit: onSubmitCb }) => {
       language: bookCreation.stepTwoDetails?.language ?? '',
       translator: bookCreation.stepTwoDetails?.translator ?? '',
       form: bookCreation.stepTwoDetails?.format ?? '',
-      pagesCount: bookCreation.stepTwoDetails?.pagesCount ?? '',
+      pagesCount: Number.isNaN(bookCreation.stepTwoDetails?.pagesCount)
+        ? ''
+        : bookCreation.stepTwoDetails?.pagesCount ?? '',
       genre: bookCreation.stepThreeDetails?.genre ?? '',
       imageUrl: '',
     },
