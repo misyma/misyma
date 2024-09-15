e0da3525ee740451c41bf0e57de8eb216c867193
Jakub Barczewski <jakub_barczewski@protonmail.com>
2024-09-15 18:04:34 +0200

    chore: authors sorting

--- apps/frontend/src/modules/author/api/user/queries/findAuthorsQuery/findAuthors.ts
+++ apps/frontend/src/modules/author/api/user/queries/findAuthorsQuery/findAuthors.ts
@@ -1,47 +1,53 @@
-import { FindAuthorsQueryParams, FindAuthorsResponseBody } from '@common/contracts';
+import {
+	FindAuthorsQueryParams,
+	FindAuthorsResponseBody,
+	SortingType,
+} from '@common/contracts';
 import { HttpService } from '../../../../../core/services/httpService/httpService';
 
 type Payload = FindAuthorsQueryParams & {
-  accessToken: string;
+	accessToken: string;
 };
 
 export const findAuthors = async (values: Payload) => {
-  const { name, page, pageSize, ids, accessToken } = values;
+	const { name, page, pageSize, ids, accessToken } = values;
 
-  const query: Record<string, string> = {};
+	const query: Record<string, string> = {
+		sortDate: SortingType.desc,
+	};
 
-  if (name) {
-    query.name = name;
-  }
+	if (name) {
+		query.name = name;
+	}
 
-  if (page) {
-    query.page = `${page}`;
-  }
+	if (page) {
+		query.page = `${page}`;
+	}
 
-  if (pageSize) {
-    query.pageSize = `${pageSize}`;
-  }
+	if (pageSize) {
+		query.pageSize = `${pageSize}`;
+	}
 
-  const customQueryAppend: Array<[string, string]> = [];
+	const customQueryAppend: Array<[string, string]> = [];
 
-  if (ids && !name) {
-    for (const id of ids) {
-      customQueryAppend.push([`ids`, id]);
-    }
-  }
+	if (ids && !name) {
+		for (const id of ids) {
+			customQueryAppend.push([`ids`, id]);
+		}
+	}
 
-  const response = await HttpService.get<FindAuthorsResponseBody>({
-    url: '/authors',
-    queryParams: query,
-    customQueryAppend,
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
+	const response = await HttpService.get<FindAuthorsResponseBody>({
+		url: '/authors',
+		queryParams: query,
+		customQueryAppend,
+		headers: {
+			Authorization: `Bearer ${accessToken}`,
+		},
+	});
 
-  if (!response.success) {
-    throw new Error('Error');
-  }
+	if (!response.success) {
+		throw new Error('Error');
+	}
 
-  return response.body;
+	return response.body;
 };
