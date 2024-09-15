0d2382efd0a4dd67776bbec2df544b5c39745a55
Michal Cieslar <michalovskyyy@gmail.com>
2024-09-15 19:42:42 +0200

    feat: update deps

--- apps/backend/src/libs/uuid/services/uuidService/uuidServiceImpl.ts
+++ apps/backend/src/libs/uuid/services/uuidService/uuidServiceImpl.ts
@@ -1,9 +1,9 @@
-import { v4 as uuidv4 } from 'uuid';
+import { randomUUID } from 'node:crypto';
 
 import { type UuidService } from './uuidService.js';
 
 export class UuidServiceImpl implements UuidService {
   public generateUuid(): string {
-    return uuidv4();
+    return randomUUID();
   }
 }
