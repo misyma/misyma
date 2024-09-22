8126aad3a0be7dcd86ace7208b4ba67922abc372
Jakub Barczewski <86159966+DeutscherDude@users.noreply.github.com>
2024-09-22 20:20:11 +0200

    chore: add author book search button (#597)

--- apps/frontend/src/modules/author/components/authorTable/authorTableColumns.tsx
+++ apps/frontend/src/modules/author/components/authorTable/authorTableColumns.tsx
@@ -1,11 +1,65 @@
-import { type ColumnDef } from '@tanstack/react-table';
+import { CellContext, type ColumnDef } from '@tanstack/react-table';
 import { Author } from '@common/contracts';
 import { HiCheckCircle } from 'react-icons/hi';
 import { HiXCircle } from 'react-icons/hi';
 import { DeleteAuthorModal } from '../deleteAuthorModal/deleteAuthorModal';
 import { UpdateAuthorModal } from '../updateAuthorModal/updateAuthorModal';
 import { TableHeader } from '../../../common/components/tableHeader/tableHeader';
-import { HiPencil } from 'react-icons/hi2';
+import { HiBookOpen, HiPencil } from 'react-icons/hi2';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '../../../common/components/tooltip/tooltip';
+import { Button } from '../../../common/components/button/button';
+import { FC } from 'react';
+import { useNavigate } from '@tanstack/react-router';
+
+const ActionsCell: FC<CellContext<Author, unknown>> = ({ row }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex items-center gap-2">
+      <TooltipProvider delayDuration={0}>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              onClick={() =>
+                navigate({
+                  to: '/admin/tabs/books/',
+                  search: {
+                    authorIds: row.original.id,
+                    page: 1,
+                    pageSize: 10,
+                  },
+                })
+              }
+              variant="none"
+              size="custom"
+            >
+              <HiBookOpen className="w-8 h-8 text-primary"></HiBookOpen>
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Zobacz książki tego Autora</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      <UpdateAuthorModal
+        trigger={<HiPencil className="text-primary text-3xl" />}
+        authorId={row.original.id}
+        authorName={row.original.name}
+        isApproved={row.original.isApproved}
+        onMutated={() => {}}
+      />
+      <DeleteAuthorModal
+        authorId={row.original.id}
+        authorName={row.original.name}
+      />
+    </div>
+  );
+};
 
 export const columns: ColumnDef<Author>[] = [
   {
@@ -48,22 +102,6 @@ export const columns: ColumnDef<Author>[] = [
     minSize: 75,
     size: 125,
     maxSize: 150,
-    cell: ({ row }): JSX.Element => {
-      return (
-        <div className="flex items-center gap-2">
-          <UpdateAuthorModal
-            trigger={<HiPencil className="text-primary text-3xl" />}
-            authorId={row.original.id}
-            authorName={row.original.name}
-            isApproved={row.original.isApproved}
-            onMutated={() => {}}
-          />
-          <DeleteAuthorModal
-            authorId={row.original.id}
-            authorName={row.original.name}
-          />
-        </div>
-      );
-    },
+    cell: ActionsCell,
   },
 ];
