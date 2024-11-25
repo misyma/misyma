0d761ce7891b25cf5539a19a59b2331b3ab89c84
Michal Cieslar <michalovskyyy@gmail.com>
2024-11-25 12:48:50 +0100

    feat: migrate scraper to use uuidv7

--- apps/scraper/src/libs/uuid/uuidService.ts
+++ apps/scraper/src/libs/uuid/uuidService.ts
@@ -1,7 +1,7 @@
-import { randomUUID } from 'node:crypto';
+import { v7 as uuidv7 } from 'uuid';
 
 export class UuidService {
   public generateUuid(): string {
-    return randomUUID();
+    return uuidv7();
   }
 }
