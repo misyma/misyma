1e32edcca0ea31cb59ff0f9ac2081f251131e2a8
Jakub Barczewski <86159966+DeutscherDude@users.noreply.github.com>
2024-11-28 15:10:30 +0100

    chore: update tooltips, increase isbn column size (#663)

---
+++ apps/frontend/src/modules/book/components/truncatedTextTooltip/truncatedTextTooltip.tsx
@@ -0,0 +1,58 @@
+import React, {
+  cloneElement,
+  FC,
+  Fragment,
+  isValidElement,
+  PropsWithChildren,
+  RefObject,
+  useRef,
+} from 'react';
+import { useIsTruncated } from '../../../common/hooks/useIsTruncated';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '../../../common/components/tooltip/tooltip';
+
+export const TruncatedTextTooltip: FC<
+  PropsWithChildren<{
+    text: string;
+  }>
+> = ({ text, children }) => {
+  const parentRef = useRef<HTMLElement>(null);
+
+  const { isTruncated } = useIsTruncated({
+    parentRef,
+    text,
+  });
+
+  const childElement = React.Children.only(children);
+
+  if (!isValidElement(childElement)) {
+    return childElement;
+  }
+
+  const elementWithRef = cloneElement(childElement, {
+    ...childElement.props,
+    ref: parentRef as RefObject<HTMLElement>,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } as React.DetailedReactHTMLElement<any, HTMLElement>);
+
+  return (
+    <Fragment>
+      {isTruncated ? (
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>{elementWithRef}</TooltipTrigger>
+            <TooltipContent className="max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl">
+              <p className="whitespace-normal break-words">{text}</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      ) : (
+        elementWithRef
+      )}
+    </Fragment>
+  );
+};
