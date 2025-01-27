65f8d820978646a6d2d9e9e89840fd3069903e08
Jakub Barczewski <jakub_barczewski@protonmail.com>
2025-01-27 21:03:05 +0100

    chore: add search back in

--- apps/frontend/src/routes/shelves/index.tsx
+++ apps/frontend/src/routes/shelves/index.tsx
@@ -1,3 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { z } from 'zod';
 
-export const Route = createFileRoute('/shelves/')({});
+const searchParamsSchema = z.object({
+  name: z.string().optional().catch(''),
+});
+
+export const Route = createFileRoute('/shelves/')({
+  validateSearch: searchParamsSchema,
+});
