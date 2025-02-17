168083d248be22c0755256d5a406c7ae6384dc93
DeutscherDude <kolokius@gmail.com>
2025-02-17 22:08:27 +0100

    chore: minor refactor

---
+++ apps/frontend/src/modules/bookshelf/components/bookshelfSearchBookInput/bookshelfSearchBookInput.tsx
@@ -0,0 +1,35 @@
+import { useNavigate, useSearch } from '@tanstack/react-router';
+import { useEffect, useState } from 'react';
+
+import { Input } from '../../../common/components/input/input';
+import useDebounce from '../../../common/hooks/useDebounce';
+import { type BookshelfBooksSearchParams } from '../bookshelfBooksFiltersBar/bookshelfBooksFiltersBar';
+
+export const BookshelfSearchButtonInput = () => {
+  const { title } = useSearch({ from: '/shelves/bookshelf/$bookshelfId' }) as BookshelfBooksSearchParams;
+  const [value, setValue] = useState(title);
+
+  const navigate = useNavigate();
+
+  const debouncedValue = useDebounce(value, 300);
+
+  useEffect(() => {
+    navigate({
+      to: '',
+      search: {
+        title: debouncedValue,
+      },
+    });
+  }, [debouncedValue, navigate]);
+
+  return (
+    <Input
+      containerClassName="p-0 h-10"
+      iSize="lg"
+      placeholder="Wyszukaj książkę"
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+      className="w-full h-10"
+    />
+  );
+};
