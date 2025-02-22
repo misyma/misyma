1983c7bd23f0687c020d669172279e3f2eab9707
DeutscherDude <kolokius@gmail.com>
2025-02-22 12:48:19 +0100

    wip: rough skeleton

--- apps/frontend/src/modules/author/api/user/queries/findAuthorsQuery/findAuthorsQuery.ts
+++ apps/frontend/src/modules/author/api/user/queries/findAuthorsQuery/findAuthorsQuery.ts
@@ -1,4 +1,10 @@
-import { type UseQueryOptions, keepPreviousData, useQuery } from '@tanstack/react-query';
+import {
+  type UseQueryOptions,
+  infiniteQueryOptions,
+  keepPreviousData,
+  useInfiniteQuery,
+  useQuery,
+} from '@tanstack/react-query';
 import { useSelector } from 'react-redux';
 
 import { type FindAuthorsResponseBody } from '@common/contracts';
@@ -49,4 +55,57 @@ export const useFindAuthorsQuery = ({ name, all = false, page, pageSize, ids, ..
     ...options,
     placeholderData: keepPreviousData,
   });
+};
+
+type InfinitePayload = {
+  accessToken: string;
+  name?: string;
+  all?: boolean;
+  ids?: string[];
+  page?: number;
+  pageSize?: number;
+};
+
+const getAuthorsInfiniteQueryOptions = ({ accessToken, name, page, ids, pageSize }: InfinitePayload) =>
+  infiniteQueryOptions({
+    queryKey: [AuthorsApiQueryKeys.findAuthorsQuery, 'infinite-query', name, `${page}`, `${ids?.join(',')}`],
+    queryFn: ({ pageParam }) =>
+      findAuthors({
+        accessToken: accessToken as string,
+        name,
+        page: pageParam as number,
+        ids,
+        pageSize,
+      }),
+    initialPageParam: page,
+    placeholderData: keepPreviousData,
+    getNextPageParam: (lastPage) => {
+      if (!lastPage) {
+        return undefined;
+      }
+
+      if (lastPage.metadata.total === 0) {
+        return undefined;
+      }
+
+      const totalPages = Math.ceil(lastPage.metadata.total / lastPage.metadata.pageSize);
+
+      if (lastPage.metadata.page === totalPages) {
+        return undefined;
+      }
+
+      return lastPage.metadata.page + 1;
+    },
+    getPreviousPageParam: (lastPage) => {
+      if (lastPage.metadata.page > 1) {
+        return lastPage.metadata.page - 1;
+      }
+
+      return undefined;
+    },
+    enabled: !!accessToken,
+  });
+
+export const useFindAuthorsInfiniteQuery = (payload: InfinitePayload) => {
+  return useInfiniteQuery(getAuthorsInfiniteQueryOptions(payload));
 };
