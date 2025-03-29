5f0580923b375ce1a1a9a0b5915228d783df6a26
Michal Cieslar <michalovskyyy@gmail.com>
2025-03-29 19:40:36 +0100

    fix: fix eslint

--- apps/backend/src/modules/userModule/infrastructure/repositories/emailEventRepository/mappers/emailEventMapper/emailEventMapper.ts
+++ apps/backend/src/modules/userModule/infrastructure/repositories/emailEventRepository/mappers/emailEventMapper/emailEventMapper.ts
@@ -1,6 +1,6 @@
+import { type EmailType } from '../../../../../../../common/types/emailType.js';
 import { EmailEvent } from '../../../../../domain/entities/emailEvent/emailEvent.js';
 import { type EmailEventStatus } from '../../../../../domain/entities/emailEvent/types/emailEventStatus.js';
-import { type EmailType } from '../../../../../../../common/types/emailType.js';
 import { type EmailEventRawEntity } from '../../../../databases/userDatabase/tables/emailEventTable/emailEventRawEntity.js';
 
 export class EmailEventMapper {
