0a52045e7a65e532dc6628b0575bdf2218554523
DeutscherDude <kolokius@gmail.com>
2025-04-19 00:07:11 +0200

    feat: remove dead code

--- apps/frontend/src/routes/mybooks/search/result/index.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-
-import { searchResultSchema } from '../../../../modules/book/components/pages/schemas/searchResultPageSchema';
-
-export const Route = createFileRoute('/mybooks/search/result/')({
-  validateSearch: (search) => searchResultSchema.parse(search),
-});
