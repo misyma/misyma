9581d906e21a0e728ef7d48ff981ef2f39446486
Jakub Barczewski <86159966+DeutscherDude@users.noreply.github.com>
2025-05-06 11:41:08 +0200

    chore: adjust modals behaviour (#959)

--- apps/frontend/src/modules/book/components/organisms/createChangeRequestModal/createChangeRequestModal.tsx
+++ apps/frontend/src/modules/book/components/organisms/createChangeRequestModal/createChangeRequestModal.tsx
@@ -4,6 +4,7 @@ import { HiArrowsRightLeft } from 'react-icons/hi2';
 import { CreateChangeRequestForm } from '../../../../bookChangeRequests/components/organisms/createChangeRequestForm/createChangeRequestForm';
 import { Dialog, DialogContent, DialogHeader, DialogTrigger } from '../../../../common/components/dialog/dialog';
 import { BookDetailsChangeRequestProvider } from '../../../context/bookDetailsChangeRequestContext/bookDetailsChangeRequestContext';
+import { ScrollArea } from '../../../../common/components/scrollArea/scroll-area';
 
 interface Props {
   bookId: string;
@@ -26,13 +27,15 @@ export const CreateChangeRequestModal: FC<Props> = ({ bookId }) => {
         <DialogContent className="p-16">
           <>
             <DialogHeader className="flex justify-center items-center text-xl font-semibold">
-              Prośba o zmianę danych w bazie
+              Prośba o zmianę danych w bazie XD2
             </DialogHeader>
-            <CreateChangeRequestForm
-              bookId={bookId}
-              onCancel={() => setIsOpen(false)}
-              onSubmit={() => setIsOpen(false)}
-            />
+            <ScrollArea className='w-full h-[300px] sm:h-[500px] md:h-[700px]'>
+              <CreateChangeRequestForm
+                bookId={bookId}
+                onCancel={() => setIsOpen(false)}
+                onSubmit={() => setIsOpen(false)}
+              />
+            </ScrollArea>
           </>
         </DialogContent>
       </Dialog>
